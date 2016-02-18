6a1c60310b9742d4522e74e57e0c935cee32da18
Brian Batchelor <batchbk@gmail.com>
2016-02-17 22:26:54 -0500

    Added Step 2

---
+++ Build_it_Bigger/FinalProject/androidjokes/src/main/java/com/udacity/gradle/builditbigger/androidlib/JokeActivity.java
@@ -0,0 +1,26 @@
+package com.udacity.gradle.builditbigger.androidlib;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class JokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            String joke = intent.getStringExtra("joke");
+            joke = joke != null && !joke.isEmpty() ? joke : "no jokes";
+
+            Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
--- Build_it_Bigger/FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ Build_it_Bigger/FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.udacity.gradle.builditbigger.androidlib.JokeActivity;
 import com.udacity.gradle.builditbigger.javalib.JavaJokes;
 
 
@@ -44,11 +47,11 @@ public class MainActivity extends ActionBarActivity {
         JavaJokes javaJokes = new JavaJokes();
         String joke = javaJokes.getJoke();
 
-        if(joke != null && !joke.isEmpty()) {
-            Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "no jokes", Toast.LENGTH_SHORT).show();
-        }
+        joke = joke != null && !joke.isEmpty() ? joke : "no jokes";
+
+        Intent jokeInent = new Intent(this,JokeActivity.class );
+        jokeInent.putExtra("joke",joke);
+        startActivity(jokeInent);
     }
 
 
